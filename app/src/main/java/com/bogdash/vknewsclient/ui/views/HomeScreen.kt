3c391aca766130b2086a8ca0f0229a163f091a7b
bogdash <ms.anastasia.bogdanova@gmail.com>
2025-12-04 18:04:39 +0300

    5.5 Создание экрана комментариев

--- app/src/main/java/com/bogdash/vknewsclient/ui/views/HomeScreen.kt
+++ app/src/main/java/com/bogdash/vknewsclient/ui/views/HomeScreen.kt
@@ -1,22 +1,11 @@
 package com.bogdash.vknewsclient.ui.views
 
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.VisibilityThreshold
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.SwipeToDismissBox
-import androidx.compose.material3.SwipeToDismissBoxValue
-import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 import com.bogdash.vknewsclient.ViewModel
+import com.bogdash.vknewsclient.domain.PostComment
+import com.bogdash.vknewsclient.ui.CommentsScreen
 
 @Composable
 fun HomeScreen(
@@ -24,49 +13,61 @@ fun HomeScreen(
     viewModel: ViewModel
 ) {
     val feedPosts = viewModel.feedPosts.observeAsState(listOf())
-    LazyColumn(
-        contentPadding = PaddingValues(
-            top = paddingValues.calculateTopPadding(),
-            bottom = paddingValues.calculateBottomPadding() + 16.dp,
-            start = 8.dp,
-            end= 8.dp
-        ),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(items = feedPosts.value, key = {it.id}) { feedPost ->
-            val dismissState = rememberSwipeToDismissBoxState(
-                confirmValueChange = { value ->
-                    val isDismissed = value in setOf(
-                        SwipeToDismissBoxValue.EndToStart
-                    )
-
-                    if (isDismissed) {
-                        viewModel.remove(feedPost)
-                    }
-                    return@rememberSwipeToDismissBoxState isDismissed
-                }
-            )
-            SwipeToDismissBox(
-                modifier = Modifier.animateItem(
-                    fadeInSpec = null,
-                    fadeOutSpec = null,
-                    placementSpec = spring(
-                        stiffness = Spring.StiffnessMediumLow,
-                        visibilityThreshold = IntOffset.VisibilityThreshold
-                    )
-                ),
-                state = dismissState,
-                enableDismissFromEndToStart = true,
-                enableDismissFromStartToEnd = false,
-                backgroundContent = {}
-            ) {
-                PostCard(
-                    feedPost = feedPost,
-                    onStatisticsItemClickListener = { statisticItem ->
-                        viewModel.updateCount(feedPost, statisticItem)
-                    }
+    if (feedPosts.value.isNotEmpty()) {
+        val comments = mutableListOf<PostComment>().apply {
+            repeat(20) {
+                add(
+                    PostComment(id = it)
                 )
             }
         }
+        CommentsScreen(feedPost = feedPosts.value[0], comments = comments)
     }
+
+
+//    LazyColumn(
+//        contentPadding = PaddingValues(
+//            top = paddingValues.calculateTopPadding(),
+//            bottom = paddingValues.calculateBottomPadding() + 16.dp,
+//            start = 8.dp,
+//            end= 8.dp
+//        ),
+//        verticalArrangement = Arrangement.spacedBy(8.dp)
+//    ) {
+//        items(items = feedPosts.value, key = {it.id}) { feedPost ->
+//            val dismissState = rememberSwipeToDismissBoxState(
+//                confirmValueChange = { value ->
+//                    val isDismissed = value in setOf(
+//                        SwipeToDismissBoxValue.EndToStart
+//                    )
+//
+//                    if (isDismissed) {
+//                        viewModel.remove(feedPost)
+//                    }
+//                    return@rememberSwipeToDismissBoxState isDismissed
+//                }
+//            )
+//            SwipeToDismissBox(
+//                modifier = Modifier.animateItem(
+//                    fadeInSpec = null,
+//                    fadeOutSpec = null,
+//                    placementSpec = spring(
+//                        stiffness = Spring.StiffnessMediumLow,
+//                        visibilityThreshold = IntOffset.VisibilityThreshold
+//                    )
+//                ),
+//                state = dismissState,
+//                enableDismissFromEndToStart = true,
+//                enableDismissFromStartToEnd = false,
+//                backgroundContent = {}
+//            ) {
+//                PostCard(
+//                    feedPost = feedPost,
+//                    onStatisticsItemClickListener = { statisticItem ->
+//                        viewModel.updateCount(feedPost, statisticItem)
+//                    }
+//                )
+//            }
+//        }
+//    }
 }
